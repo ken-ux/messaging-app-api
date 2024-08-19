b824e1d05b47fbd227f93893eacbb6dd4548839e
Kenny Nguyen <sokenns@gmail.com>
2024-08-19 10:52:38 -0700

    Add code for sending JWTs

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// "context"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,10 +13,11 @@ import (
 
 	// "github.com/jackc/pgx/v5/pgxpool"
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 )
 
-type NewUser struct {
+type User struct {
 	Username string `json:"username" validate:"required,min=5,max=20,alphanum"`
 	Password string `json:"password" validate:"required,min=5,max=20"`
 }
@@ -57,7 +59,7 @@ func main() {
 }
 
 func registerUser(c *gin.Context) {
-	var user NewUser
+	var user User
 
 	// Bind JSON fields from form data.
 	if err := c.BindJSON(&user); err != nil {
@@ -65,12 +67,8 @@ func registerUser(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(user)
-
-	// Validate form inputs for invalid characters and check if username is taken.
+	// Validate form input.
 	validate := validator.New(validator.WithRequiredStructEnabled())
-
-	// Validate the User struct
 	if err := validate.Struct(user); err != nil {
 		// Validation failed, handle the error
 		errors := err.(validator.ValidationErrors)
@@ -78,13 +76,35 @@ func registerUser(c *gin.Context) {
 		return
 	}
 
+	fmt.Println(user)
+
 	// Bind hashed password to user variable.
+
+	fmt.Println(user)
 
 	// Send query to backend to register user.
 
 	// Send JWT to client.
+	// Load secret and cast from string to []byte.
+	secret := os.Getenv("SECRET")
+	key, err := base64.RawStdEncoding.DecodeString(secret)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, fmt.Sprintf("JWT error: %v", err))
+	}
 
-	c.IndentedJSON(http.StatusOK, "success")
+	// Encode user-specific information into token.
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"sub": user.Username,
+		})
+
+	// Sign token with key.
+	s, err := t.SignedString(key)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, fmt.Sprintf("JWT error: %v", err))
+	}
+
+	c.IndentedJSON(http.StatusOK, s)
 }
 
 func loginUser(c *gin.Context) {
