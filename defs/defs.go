a2b8cc5328a008196dd380f2e0644bf2c9c4c1fe
Kenny Nguyen <sokenns@gmail.com>
2024-09-04 02:20:03 -0700

    Omit empty password field from JSON object

--- defs/defs.go
+++ defs/defs.go
@@ -2,5 +2,5 @@ package defs
 
 type User struct {
 	Username string `json:"username" validate:"required,min=5,max=20,alphanum"`
-	Password string `json:"password" validate:"required,min=5,max=20"`
+	Password string `json:"password,omitempty" validate:"required,min=5,max=20"`
 }
