e6b0d52ae7e39cb5fc43ad4fb06ed0ac18754df5
Kenny Nguyen <sokenns@gmail.com>
2024-09-17 16:57:37 -0700

    Add message struct to definitions

--- defs/defs.go
+++ defs/defs.go
@@ -1,6 +1,15 @@
 package defs
 
+import "time"
+
 type User struct {
 	Username string `json:"username" validate:"required,min=5,max=20,alphanum"`
 	Password string `json:"password,omitempty" validate:"required,min=5,max=20"`
+}
+
+type Message struct {
+	Sender        string    `json:"sender" binding:"required"`
+	Recipient     string    `json:"recipient" binding:"required"`
+	Message_Body  string    `json:"message_body" binding:"required"`
+	Creation_Date time.Time `json:"creation_date" binding:"required"`
 }
